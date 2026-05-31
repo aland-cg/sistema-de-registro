c3d6fe803c30a040be9d9f1ed884db988e5a5f5c
aland-cg <143722863+aland-cg@users.noreply.github.com>
2026-05-31 05:37:09 +0000

    mejora organizacion visual del formulario

--- app.py
+++ app.py
@@ -1,49 +1,107 @@
+# Importar herramientas necesarias de Flask
 from flask import Flask, render_template, request
+# Importar SQLite para guardar datos
 import sqlite3
 
+# Crear instancia principal de la aplicación Flask
 app = Flask(__name__)
-
-# Crear tabla si no existe
+#inicializa la base de datos
 def init_db():
-
+# crea conexion con la base de datos
     conn = sqlite3.connect("database.db")
+#ejecuta comandos de sql
     cursor = conn.cursor()
-
+# Crear tabla principal de pacientes si aún no existe
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS pacientes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT NOT NULL,
-        edad INTEGER NOT NULL
-    )
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT,
+    edad INTEGER,
+    ta TEXT,
+    fc INTEGER,
+    spo2 INTEGER,
+    glucosa INTEGER,
+    temp REAL
+);
     """)
-
+#sirve para guardar cambios 
     conn.commit()
+#cierra la conexion lo que evita errores de escritura una vez que se capturan los datos
     conn.close()
 
 init_db()
 
+# Ruta principal para registrar pacientes
 @app.route("/", methods=["GET", "POST"])
 def home():
 
     if request.method == "POST":
 
-        nombre = request.form["nombre"]
-        edad = request.form["edad"]
+        paciente = {
+        "nombre": request.form["nombre"],
+        "edad": request.form["edad"],
+        "ta": request.form["ta"],
+        "fc": request.form["fc"],
+        "spo2": request.form["spo2"],
+        "glucosa": request.form["glucosa"],
+        "temp": request.form["temp"]
+    }
+
+    # Validaciones
+
+        edad = int(paciente["edad"])
+
+        if edad < 0 or edad > 120:
+            return "Error: Edad fuera de rango permitido."
+
+        fc = int(paciente["fc"])
+
+        if fc < 20 or fc > 250:
+            return "Error: Frecuencia cardiaca fuera de rango permitido."
+
+        spo2 = int(paciente["spo2"])
+
+        if spo2 < 50 or spo2 > 100:
+            return "Error: Saturación fuera de rango permitido."
+
+        glucosa = int(paciente["glucosa"])
+
+        if glucosa < 20 or glucosa > 800:
+            return "Error: Glucosa fuera de rango permitido."
+
+        temp = float(paciente["temp"])
+
+        if temp < 30 or temp > 45:
+            return "Error: Temperatura fuera de rango permitido."
 
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
-
+# Insertar datos clínicos del paciente en SQLite
         cursor.execute(
-            "INSERT INTO pacientes (nombre, edad) VALUES (?, ?)",
-            (nombre, edad)
-        )
+    """
+    INSERT INTO pacientes
+    (nombre, edad, ta, fc, spo2, glucosa, temp)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """,
+    (
+        paciente["nombre"],
+        paciente["edad"],
+        paciente["ta"],
+        paciente["fc"],
+        paciente["spo2"],
+        paciente["glucosa"],
+        paciente["temp"]
+    )
+)
 
         conn.commit()
         conn.close()
-
-        return f"Paciente registrado correctamente: {nombre}"
+#devuelven respuestas al usuario
+        return f"Paciente registrado correctamente: {paciente['nombre']}"
+#muestra el formilario html
     return render_template("index.html")
-
+#consulta registros guardados
+#Iniciar servidor Flask en modo desarrollo
 @app.route("/pacientes")
 def pacientes():
 
@@ -57,6 +115,6 @@ def pacientes():
     conn.close()
 
     return render_template("pacientes.html", pacientes=datos)
-
+## Iniciar servidor Flask en modo desarrollo
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
